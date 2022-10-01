c97bc0a4421fb816541f4728979e7608ca9aec9d
Mahedi hasan <mahedi2112p@gmail.com>
2022-10-01 23:35:07 +0600

    first added

--- js/selected.js
+++ js/selected.js
@@ -1,20 +1,15 @@
-document.getElementById("players").addEventListener("click", function (event) {
-    event.stopPropagation();
-    const playerList = document.getElementById("select-player-list");
-    const name = document.getElementById("name").innerText;
+function playerList(playerName) {
+    const playerContainer = document.getElementById("select-player-list");
     const li = document.createElement("li");
-    li.innerText = name;
-    li.classList.add("ol");
-    playerList.appendChild(li);
-    console.log(event, playerList, name);
+    li.innerText = playerName;
+    playerContainer.appendChild(li);
+    console.log(playerName);
+}
+
+
+document.getElementById("players").addEventListener("click", function (event) {
+    const playerName = event.target.parentNode.childNodes[1].innerText;
+    playerList(playerName);
+
 })
 
-/* document.getElementById('add-item6').addEventListener('click', function () {
-    const ulContainer = document.getElementById('list-container');
-    const li = document.createElement('li');
-    const name6 = document.getElementById('name6').innerText;
-    li.innerText = name6;
-    li.classList.add('item6');
-    ulContainer.appendChild(li);
-
-}) */
