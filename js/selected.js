a22fe8988a532a0e1aa47f99ae7463b7e66ecfbf
Mahedi hasan <mahedi2112p@gmail.com>
2022-10-02 11:37:48 +0600

    first commit

--- js/selected.js
+++ js/selected.js
@@ -1,14 +1,19 @@
 function playerList(playerName) {
+    if (playerName == '') {
+        alert('Already added');
+        return;
+    }
     const playerContainer = document.getElementById("select-player-list");
     const li = document.createElement("li");
     li.innerText = playerName;
     playerContainer.appendChild(li);
-    console.log(playerName);
+    // console.log(playerName);
 }
 
 
 document.getElementById("players").addEventListener("click", function (event) {
     const playerName = event.target.parentNode.childNodes[1].innerText;
+    console.log(event.target.parentNode.childNodes);
     playerList(playerName);
 
 })
