646e2843ac3080cc6cec04bf4f9e4a506a521534
Mahedi hasan <mahedi2112p@gmail.com>
2022-10-02 20:42:56 +0600

    total calculation added

--- js/selected.js
+++ js/selected.js
@@ -1,57 +1,69 @@
-let playerCount;
-
 function playerList(playerName) {
-    // console.log(playerName);
-    if (playerName == " ") {
-        alert('Already added');
-        return;
+    if (playerName == '') {
+        alert('Already Added');
+        return
     }
-
-    const playerContainer = document.getElementById("select-player-list");
-    const li = document.createElement("li");
-    li.innerText = "" + playerName;
-    li.classList.add("singlePlayer");
-
-    playerContainer.appendChild(li);
-
-    const player = document.querySelectorAll(".singlePlayer");
-    console.log(player.length);
-    playerCount = player.length;
-    // console.log(playerCount)
-
+    const listContainer = document.getElementById('select-player-list');
+    const li = document.createElement('li');
+    li.innerText = " " + playerName;
+    li.classList.add('singlePlayer')
+    listContainer.appendChild(li)
+    const player = document.querySelectorAll('.singlePlayer');
+    const playerCount = player.length;
     if (playerCount > 5) {
-        alert("You added maximum Players");
-        playerContainer.removeChild(li);
-        return 5;
+        alert('you added maximum players')
+        listContainer.removeChild(li)
+        return 5
     }
-    return playerCount;
+    return playerCount
 }
 
+//getInputValueById
+function getInputValueById(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputFieldString = inputField.value;
+    const inputValue = parseFloat(inputFieldString)
+    if (isNaN(inputValue)) {
+        alert('Input Field Should not be string or Blank')
+        return 0
+    }
+    return inputValue
+}
+// Common Function for All Total Set
+function setTotalTextById(elementId, total) {
+    const TotalTextElement = document.getElementById(elementId);
 
+    TotalTextElement.innerText = total;
+}
+let playerCount;
+let playerExpense;
 
-document.getElementById("players").addEventListener("click", function (event) {
-    const playerName = event.target.parentNode.childNodes[1].innerText;
+// player select handler
+document.getElementById('players').addEventListener('click', function (e) {
+    const playerName = e.target.parentNode.childNodes[1].innerText;
+    console.log(playerName);
+    e.target.setAttribute('disabled', true);
+    playerCount = playerList(playerName)
+})
 
-    event.target.setAttribute("disabled", true);
-    //
-    // const player = playerList(playerName);
-    // console.log(player);
-    // 
-    playerList(playerName);
-
+// EventListener for calculate Player Expense
+document.getElementById('player-btn').addEventListener('click', function (e) {
+    const perPlayerBudget = getInputValueById('player-budget')
+    playerExpense = perPlayerBudget * playerCount;
+    if (isNaN(playerExpense)) {
+        return 0
+    }
+    setTotalTextById('player-expense', playerExpense)
 })
 
 
-// Calculation
-document.getElementById("player-btn").addEventListener("click", function () {
-    const playerBudget = document.getElementById("player-budget");
-    const playerBudgetString = playerBudget.value;
-    const previousBudget = parseFloat(playerBudgetString);
-
-    const player = playerList()
-    console.log(playerCount);
-    const total = player * previousBudget;
-    // console.log(total);
-
+// EventListener For Total Calculation
+document.getElementById('total-calculate').addEventListener('click', function (e) {
+    const managerBudget = getInputValueById('manager-field');
+    const coachBudget = getInputValueById('coach-field');
+    const totalExpense = playerExpense + managerBudget + coachBudget;
+    if (isNaN(totalExpense)) {
+        return 0
+    }
+    setTotalTextById('totalExpense', totalExpense)
 })
-
