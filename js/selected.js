c958e2187b5966db951d435fcdd4cdb58bfaee2d
Mahedi hasan <mahedi2112p@gmail.com>
2022-10-02 12:05:24 +0600

    first added

--- js/selected.js
+++ js/selected.js
@@ -3,9 +3,11 @@ function playerList(playerName) {
         alert('Already added');
         return;
     }
+
     const playerContainer = document.getElementById("select-player-list");
     const li = document.createElement("li");
     li.innerText = playerName;
+
     playerContainer.appendChild(li);
     // console.log(playerName);
 }
@@ -13,7 +15,9 @@ function playerList(playerName) {
 
 document.getElementById("players").addEventListener("click", function (event) {
     const playerName = event.target.parentNode.childNodes[1].innerText;
-    console.log(event.target.parentNode.childNodes);
+    console.log(event.target);
+    event.target.setAttribute("disabled", true);
+    // console.log(event.target.parentNode.childNodes);
     playerList(playerName);
 
 })
